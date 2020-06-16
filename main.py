7477a3daef77b8970391c1503e4b92ef4ece60b3
26kimayam <26kimayam@gmail.com>
2020-06-16 17:43:25 +0000

    Added the win game statements and finished the matching game.

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def get_choice(limit, pos, turn):
     try:
       id = int(input('Enter the {} of the {} card you would like to flip: '.format(pos,turn)))-1
     except ValueError:
-      print("That is not a number. Pleaseenter a number between 1 and {}.".format(limit))
+      print("That is not a number. Please enter a number between 1 and {}.".format(limit))
     else:
       if id >= limit:
         print('That {} is out of range. Please enter a number between 1 and {}.'.format(pos, limit))
@@ -40,6 +40,41 @@ def get_choice(limit, pos, turn):
 def main():
     make_board()
     print_board()
+    match_count = 0
+    attempts = 0
+    first_flip = True
+    while match_count < len(characters):
+      if first_flip:
+        r1 = get_choice(rows, 'row', 'first')
+        c1 = get_choice(cols, 'column', 'first')
+        if board[r1, c1, 0] == 1.0:
+          print("That card is already flipped.")
+        else:
+          first_flip = False
+          board[r1, c1, 0] = 1.0
+          print_board()
+
+      else:
+        r2 = get_choice(rows, 'row', 'second')
+        c2 = get_choice(cols, 'column', 'second')
+        if board[r2, c2, 0] == 1.0:
+          print("That card is already flipped.")
+        else:
+          attempts += 1
+          first_flip = True
+          board[r2, c2, 0] = 1.0
+          print_board()
+          if board[r1,c1,1] == board[r2,c2,1]:
+            print('You got a match!')
+            match_count += 1
+          else:
+            print('No match! Better luck next time.')
+            board[r1,c1,0] = 0.0
+            board[r2,c2,0] = 0.0
+            print_board()
+    print("You completed the game in {} attempts".format(attempts))
+
+
 
 if __name__ == '__main__':
     main()   
